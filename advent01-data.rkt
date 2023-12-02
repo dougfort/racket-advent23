26e94303072b08acca17c622583f73d8fa7e8d32
dougfort <doug.fort@gmail.com>
2023-12-02 13:19:40 -0500

    failed attempt at day 1 part 2
    The unit test passes

--- advent01-data.rkt
+++ advent01-data.rkt
@@ -4,7 +4,12 @@
 
 (require threading)
 
-(provide test-data data)
+(provide
+ part-1-test-data
+ part-2-test-data
+ part-1-data
+ part-2-data
+ )
 
 (define (split-raw raw)
   (~>
@@ -12,36 +17,62 @@
    string-trim
    (string-split "\n")))
 
-(define (numeric-chars line)
-  (for/list ([chr (in-list (string->list line))]
-             #:when (char-numeric? chr))
-    chr))
-
-(define (calibration-value-chars chrs)
-  (list (first chrs) (last chrs)))
-
-(define (parse-line line)
-  (~>
-   line
-   numeric-chars
-   calibration-value-chars
-   list->string
-   string->number))
-   
-(define (parse raw)  
-  (map parse-line (split-raw raw)))
-
-(define raw-test-data
+(define raw-part-1-test-data
   "1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet
 ")
 
-(define test-data (λ () (parse raw-test-data)))
+(define part-1-test-data (split-raw raw-part-1-test-data))
+
+(define raw-part-2-test-data
+  "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+")
+
+(define text-digits
+  (list
+   '("one" "1")
+   '("two" "2")
+   '("three" "3")
+   '("four" "4")
+   '("five"  "5")
+   '("six" "6")
+   '("seven" "7")
+   '("eight" "8")
+   '("nine" "9")))
+
+(define (replace-prefix-digit in-str)
+  (let ([match (for/last ([digit-pair (in-list text-digits)]
+                          #:when (string-prefix? in-str (first digit-pair)))
+                 digit-pair)])
+    (cond
+      [(false? match)  (values (substring in-str 1) (substring in-str 0 1))]
+      [else
+       (let ([len (string-length (first match))])
+         (values (substring in-str len) (second match) ))]))) 
+
+(define (replace-text-digits str)
+  (let loop ([in-str str]
+             [out-str ""])
+    (cond
+      [(equal? in-str "") out-str]
+      [else
+       (let-values ([(next-in next-out) (replace-prefix-digit in-str)])
+         (loop next-in (string-append out-str next-out)))])))      
+
+(define part-2-test-data (split-raw (replace-text-digits raw-part-2-test-data)))
 
 (define data-id "advent01.data")
 
-(define data (λ ()
-               (let ([raw-data (port->string (open-input-file data-id) #:close? #t)])               
-                 (parse raw-data))))
+(define data (port->string (open-input-file data-id) #:close? #t))
+
+(define part-1-data (split-raw data))
+
+(define part-2-data (split-raw (replace-text-digits data)))
--- advent01.rkt
+++ advent01.rkt
@@ -2,14 +2,35 @@
 
 ;; 
 
+(require threading)
+
 (module+ test
   (require rackunit))
 
 (require "advent01-data.rkt")
 
+(define (numeric-chars line)
+  (for/list ([chr (in-list (string->list line))]
+             #:when (char-numeric? chr))
+    chr))
+
+(define (calibration-value-chars chrs)
+  (list (first chrs) (last chrs)))
+
+(define (parse-line line)
+  (~>
+   line
+   numeric-chars
+   calibration-value-chars
+   list->string
+   string->number))   
+
 (module+ test
   (test-begin
-   (define sum-of-calibration-values (apply + (test-data)))
-   (check-equal? sum-of-calibration-values 142)))
+   (define part-1-values (apply + (map parse-line part-1-test-data)))
+   (check-equal? part-1-values 142))
+   (define part-2-values (apply + (map parse-line part-2-test-data)))
+   (check-equal? part-2-values 281))
    
-(define sum-of-calibration-values (apply + (data)))
+(define part-1-sum-of-calibration-values (apply + (map parse-line part-1-data)))
+(define part-2-sum-of-calibration-values (apply + (map parse-line part-2-data)))
