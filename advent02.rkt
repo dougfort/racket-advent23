13acb9f11b65b9438b15eb0170bfbad4b0682e2d
dougfort <doug.fort@gmail.com>
2023-12-04 06:48:58 -0500

    completed Day 2

--- advent02.rkt
+++ advent02.rkt
@@ -21,12 +21,30 @@
       [(< (cubeset-green bag) (cubeset-green cs)) #f]
       [(< (cubeset-blue bag) (cubeset-blue cs)) #f]
       [else #t])))
+
+;; compare two cubesets
+;; return a cubeset that contains the maximum if each cube type
+(define (max-cubes lhs rhs)
+  (cubeset (max (cubeset-red lhs) (cubeset-red rhs))
+           (max (cubeset-green lhs) (cubeset-green rhs))
+           (max (cubeset-blue lhs) (cubeset-blue rhs))))
+
+(define (min-cubes-for-game game)
+  (let ([max-c (for/fold ([accum (cubeset 0 0 0)])
+                         ([c (game-result-cubesets game)])
+                 (max-cubes c accum))])
+    max-c))
+
+(define (power-of-cubeset cs)
+  (* (cubeset-red cs) (cubeset-green cs) (cubeset-blue cs)))
       
 (module+ test
   (test-begin
    (define bag (cubeset 12 13 14))
-   (check-equal? 8 (apply + (possible-games bag test-data))))) 
+   (check-equal? 8 (apply + (possible-games bag test-data)))
+   (check-equal? 2286 (apply + (map power-of-cubeset (map min-cubes-for-game test-data))))))
+
 
 (define bag (cubeset 12 13 14))
 (apply + (possible-games bag data)) 
-
+(apply + (map power-of-cubeset (map min-cubes-for-game data)))
